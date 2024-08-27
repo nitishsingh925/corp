6a4ce1d022b390819beb6c44e521dec3344a0245
Nitish Singh <nitishsingh925@gmail.com>
2024-08-27 11:09:18 +0530

    add favicon and meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from "@/components/hero";
-import homeImg from "../../public/home.jpg"; // Correctly imported image
+import homeImg from "../../public/home.jpg";
 export default function Home() {
   return (
     <main>
-      This is Home
       <Hero
-        // imgData={"/home.jpg"}
         imgData={homeImg}
         imgAlt="Car Factory"
         title="Professional Cloud Hosting"
