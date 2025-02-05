4708485296ae1fe9343daf20c14b5d400e30dbc7
Nitish Singh <76509326+nitishsingh925@users.noreply.github.com>
2025-02-05 20:07:28 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 const inter = Inter({ subsets: ["latin"] });
 
+
 export const metadata: Metadata = {
   title: "corporate",
   description:
