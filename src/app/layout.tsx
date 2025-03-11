d2ae8e644b13c3bf3c40797c6e73da7e9452a15b
Nitish Singh <76509326+nitishsingh925@users.noreply.github.com>
2025-03-11 22:44:59 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "corporate",
+  
   description:
     "Welcome to our corporate site, where we provide top-notch services to our clients. Our team of experts is dedicated to delivering exceptional results and exceeding client expectations.",
 };
